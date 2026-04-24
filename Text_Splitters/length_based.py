a4630cc25c96e7334e7fe9b6b91d2ce3e0a9b611
tygopal45 <jeegopal835@gmail.com>
2026-04-24 20:11:45 +0530

    added text based splitter

--- Text_Splitters/length_based.py
+++ Text_Splitters/length_based.py
@@ -1,4 +1,10 @@
 from langchain_text_splitters import CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+
+loader = PyPDFLoader("Text_Splitters/dl-curriculum.pdf")
+docs = loader.load()
+
+
 
 text = """Space exploration has led to incredible scientific discoveries. From landing on the Moon to exploring Mars, humanity continues to push the boundaries of what’s possible beyond our planet.
 
@@ -8,16 +14,20 @@ These missions have not only expanded our knowledge of the universe but have als
 splitter = CharacterTextSplitter(
     chunk_size=100, 
     chunk_overlap=0,
-    # what is chunk overlap? 
-    # Chunk overlap refers to the number of characters that are shared between consecutive chunks when splitting a text. It helps to maintain context and continuity between chunks, especially when the text is split at natural breakpoints. For example, if you have a chunk size of 100 characters and a chunk overlap of 20 characters, the first chunk would contain characters 0-99, the second chunk would contain characters 80-179, and so on. This way, the last 20 characters of the first chunk are included in the second chunk, providing some overlap for better understanding and coherence when processing the chunks separately.
     separator=" "
 )
 
 result = splitter.split_text(text)
 
 print(result)
-
 print(len(result))
 
+
+# For dicument splitting, you can use the same splitter to split the loaded documents. 
+# The `split_documents` method will apply the splitting logic to each document in the list.
+
+result_doc = splitter.split_documents(docs)
+# for first chunk of the first document
+print(result_doc[0])
 
 
